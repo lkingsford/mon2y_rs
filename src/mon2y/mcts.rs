fc00ac1ebbcdb7c8db8b12243a16d2f5bd61ec5c
Lachlan Kingsford <lachlan@nerdygentleman.com>
2024-12-13 08:26:04 +1100

    Move calculate_best_turn out of Tree into mcts

--- src/mon2y/mcts.rs
+++ src/mon2y/mcts.rs
@@ -1,17 +1,89 @@
+use log::{debug, trace};
+
+use crate::mon2y::game::Actor;
+
 use super::game::{Action, State};
-use super::node::create_expanded_node;
+use super::node::{create_expanded_node, Node};
 use super::tree::Tree;
 use super::BestTurnPolicy;
 
-pub fn calculate_best_turn<StateType>(
+pub fn calculate_best_turn<
+    StateType: State<ActionType = ActionType>,
+    ActionType: Action<StateType = StateType>,
+>(
     iterations: usize,
     state: StateType,
     policy: BestTurnPolicy,
 ) -> <StateType as State>::ActionType
 where
-    StateType: State,
+    StateType: State<ActionType = ActionType>,
+    ActionType: Action<StateType = StateType>,
 {
     let mut root_node = create_expanded_node(state);
     let mut tree = Tree::new(root_node);
-    tree.calculate_best_turn(iterations, policy)
+
+    for iteration in 0..iterations {
+        trace!("Starting iteration {}", iteration);
+        &tree.iterate();
+    }
+    if log::log_enabled!(log::Level::Trace) {
+        tree.root.trace_log_children(0);
+    }
+    match policy {
+        BestTurnPolicy::MostVisits => {
+            if let Node::Expanded { children, .. } = &tree.root {
+                debug!(
+                    "Action, Visits, Value: {:?}",
+                    children
+                        .iter()
+                        .map(|(action, node)| (
+                            action.clone(),
+                            node.visit_count(),
+                            node.value_sum()
+                        ))
+                        .collect::<Vec<_>>()
+                );
+                // Short circuit on a winning move
+                // Implemented because (I think) the UCB formula doesn't end up prioritizing
+                // certainly winning moves, because they're already explored. Dunno if this
+                // is a cludge though.
+                let winning_moves: Vec<ActionType> = children
+                    .iter()
+                    .filter_map(|(action, node)| {
+                        if node.state().terminal() {
+                            let actor = tree.root.state().next_actor();
+                            if let Actor::Player(player_id) = actor {
+                                if let Some((index, _)) =
+                                    // Annoying - but necessary because I was dumb enough to use f64
+                                    // (otherwise, it'd be max_by_key)
+                                    node.state().reward().iter().enumerate().max_by(
+                                            |(_, a), (_, b)| {
+                                                a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Less)
+                                            },
+                                        )
+                                {
+                                    if index == player_id as usize {
+                                        return Some(action.clone());
+                                    }
+                                }
+                            }
+                        }
+                        None
+                    })
+                    .collect();
+                if let Some(action) = winning_moves.first() {
+                    return action.clone();
+                }
+
+                children
+                    .iter()
+                    .max_by_key(|(_, node)| node.visit_count())
+                    .unwrap()
+                    .0
+                    .clone()
+            } else {
+                panic!("Expected root to be an expanded node")
+            }
+        }
+    }
 }
--- src/mon2y/tree.rs
+++ src/mon2y/tree.rs
@@ -13,7 +13,7 @@ pub enum Selection<ActionType: Action> {
 }
 
 pub struct Tree<StateType: State, ActionType: Action<StateType = StateType>> {
-    root: Node<StateType, ActionType>,
+    pub root: Node<StateType, ActionType>,
     constant: f64,
 }
 
@@ -129,74 +129,6 @@ where
             trace!("After propagate");
             if log::log_enabled!(log::Level::Trace) {
                 self.root.best_pick(self.constant);
-            }
-        }
-    }
-
-    pub fn calculate_best_turn(&mut self, iterations: usize, policy: BestTurnPolicy) -> ActionType {
-        for iteration in 0..iterations {
-            trace!("Starting iteration {}", iteration);
-            &self.iterate();
-        }
-        if log::log_enabled!(log::Level::Trace) {
-            self.root.trace_log_children(0);
-        }
-        match policy {
-            BestTurnPolicy::MostVisits => {
-                if let Node::Expanded { children, .. } = &self.root {
-                    debug!(
-                        "Action, Visits, Value: {:?}",
-                        children
-                            .iter()
-                            .map(|(action, node)| (
-                                action.clone(),
-                                node.visit_count(),
-                                node.value_sum()
-                            ))
-                            .collect::<Vec<_>>()
-                    );
-                    // Short circuit on a winning move
-                    // Implemented because (I think) the UCB formula doesn't end up prioritizing
-                    // certainly winning moves, because they're already explored. Dunno if this
-                    // is a cludge though.
-                    let winning_moves: Vec<ActionType> = children
-                        .iter()
-                        .filter_map(|(action, node)| {
-                            if node.state().terminal() {
-                                let actor = self.root.state().next_actor();
-                                if let Actor::Player(player_id) = actor {
-                                    if let Some((index, _)) =
-                                        // Annoying - but necessary because I was dumb enough to use f64
-                                        // (otherwise, it'd be max_by_key)
-                                        node.state().reward().iter().enumerate().max_by(
-                                                |(_, a), (_, b)| {
-                                                    a.partial_cmp(b)
-                                                        .unwrap_or(std::cmp::Ordering::Less)
-                                                },
-                                            )
-                                    {
-                                        if index == player_id as usize {
-                                            return Some(action.clone());
-                                        }
-                                    }
-                                }
-                            }
-                            None
-                        })
-                        .collect();
-                    if let Some(action) = winning_moves.first() {
-                        return action.clone();
-                    }
-
-                    children
-                        .iter()
-                        .max_by_key(|(_, node)| node.visit_count())
-                        .unwrap()
-                        .0
-                        .clone()
-                } else {
-                    panic!("Expected root to be an expanded node")
-                }
             }
         }
     }
